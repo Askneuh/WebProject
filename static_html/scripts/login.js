f5fa0abf470f502778cab00fb557ecb461b54400
Sebastien <askneuh@gmail.com>
2025-05-24 14:14:38 +0200

    fix deployment issues

--- static_html/scripts/login.js
+++ static_html/scripts/login.js
@@ -21,23 +21,19 @@ async function login() {
             const result = await response.json();
             console.log("response ok");
             console.log(result.auth_token);
-            //localStorage.setItem('auth_token', result.auth_token); (askip ça le fait automatiquement)            // Redirect or perform other actions based on successful login
             console.log("Redirecting to index page");
             window.location.href = '/pages/index.html';
             
             
         } else if (response.status === 401) {
-            // Handle 401 Unauthorized error
             alert('Unauthorized: Invalid username or password');
         } else {
             console.log("wtf");
-            // Handle other non-successful response codes
             alert('Login failed: ' + response.statusText);
         }
     } catch (error) {
         console.log("la");
         alert('Login failed: ' + error.message);
-        // Handle other errors
     }
 }
 
